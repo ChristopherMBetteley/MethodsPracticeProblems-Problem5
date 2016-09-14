317c2e4521172c823007c857d8e9a0a0bf057456
ChristopherMBetteley <Christopher.M.Betteley@gmail.com>
2016-09-14 18:00:05 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿//Create a method named "Double" to calculate and return an integer 
+//number doubled. For example. Double(7) should return 14.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsPracticeProblems_Problem5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare variables
+            int userInputNumber;
+            int doubledNumber;
+
+            //ask user for a number to be doubled.
+            Console.WriteLine("Please provide a number to be doubled.");
+            userInputNumber = int.Parse(Console.ReadLine());
+
+            //calls the Double method and prints it's return value
+            doubledNumber = Double(userInputNumber);
+            Console.WriteLine(doubledNumber);
+        }
+
+        //method to double provided number.
+        static int Double(int originalNumber)
+        {
+            int doubled;
+            int multiplier = 2;
+
+            doubled = originalNumber * multiplier;
+            return doubled;
+        }
+    }
+}
